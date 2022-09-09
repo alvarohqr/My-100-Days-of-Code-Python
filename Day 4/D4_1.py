bf348210e819647df27dc4b65bf8b257ea54828f
alvarohqr <55456864+alvarohqr@users.noreply.github.com>
2022-09-08 21:13:36 -0700

    Update D4_1.py

--- Day 4/D4_1.py
+++ Day 4/D4_1.py
@@ -1,3 +1,19 @@
-import random as rnd
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
 
-print(rnd.randrange(1,20))
+import random 
+
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write the rest of your code below this line 👇
+coin = random.randint(-0,1)
+
+if coin == 0:
+    print("Tails")
+elif coin == 1:
+    print("Heads")
+else:
+    print("Something went wrong")
