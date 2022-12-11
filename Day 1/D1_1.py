e833d76c5bbc33de795373b97313691ce31289ab
alvarohqr <alvarohqr@gmail.com>
2022-12-10 17:53:05 -0700

    Day 11

--- Day 1/D1_1.py
+++ Day 1/D1_1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 Write a program in main.py that prints the same notes from the previous lesson 
 using what you have learnt about the Python print function.
@@ -10,17 +9,4 @@ otherwise the tests won't pass.
 
 print("Day 1 - Python Print Function")
 print('The function is declared like this:')
-=======
-'''
-Write a program in main.py that prints the same notes from the previous lesson 
-using what you have learnt about the Python print function.
-Warning: The output in your program should match the example output shown below 
-exactly, character for character, even spaces and symbols should be identical, 
-otherwise the tests won't pass.
-'''
-#Write your code below this line 👇
-
-print("Day 1 - Python Print Function")
-print('The function is declared like this:')
->>>>>>> ba2e29ddac2f6db0e1aebf50ee560d2ffe5a7d74
 print("print('what to print')")
