399a1d33614c2b6559a7318b665704583e81ed4a
alvarohqr <alvarohqr@gmail.com>
2023-03-12 16:30:26 -0700

    Tkinter, *args & **kargs

--- Day 27/D27_1.py
+++ Day 27/D27_1.py
@@ -1,14 +1,17 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Tkinter GUI")
 window.minsize(480, 480)
 
 #Label
-my_label = tkinter.Label(text="I'am a label", font=("Arial", 26, "bold"), height=4)
-my_label.pack(side="left")
+my_label = Label(text="I'am a label", font=("Arial", 26, "bold"), height=4)
+my_label.pack()
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text 2")
+
+button = Button(text="Click me")
+button.pack()
 
 window.mainloop()
