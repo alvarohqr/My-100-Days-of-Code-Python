516f12a08a46327f8433c5e7f85079ef55782454
alvarohqr <alvarohqr@gmail.com>
2023-03-12 16:40:53 -0700

    Tkinter: label, entry & button event

--- Day 27/D27_1.py
+++ Day 27/D27_1.py
@@ -5,18 +5,23 @@ window.title("Tkinter GUI")
 window.minsize(480, 480)
 
 #Label
-my_label = Label(text="I'am a label", font=("Arial", 26, "bold"), height=4)
+my_label = Label(text="I'am a label", font=("Arial", 26, "bold"))
 my_label.pack()
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text 2")
 
+#Entry
+input = Entry(width=10)
+input.pack()
+
+
 #Button
-def button_clicked():
-    global my_label
-    my_label.config(text="Button got clicked")
+def button_clicked(): 
+    my_label.config(text=input.get())
 
 button = Button(text="Click me", command=button_clicked)
 button.pack()
+
 
 window.mainloop()
