b0d8c6678b4630e8c8c9fcc45690bb26b4c63aae
alvarohqr <alvarohqr@gmail.com>
2023-03-13 12:55:20 -0700

    Tkinter: grid positioning

--- Day 27/main.py
+++ Day 27/main.py
@@ -8,17 +8,24 @@ window.title("Tkinter GUI")
 window.minsize(480, 480)
 
 #Label
-my_label = Label(text="I'am a label", font=("Arial", 26, "bold"))
+my_label = Label(text="I'am a label", font=("Arial", 12, "bold"))
 my_label["text"] = "New Text"
 my_label.config(text="New Text 2")
-my_label.pack(side="left")
+
+# my_label.pack(side="left")
+#my_label.place(x=100,y=200)
+my_label.grid(column=0, row=0)
 
 #Button
 button = Button(text="Click me", command=button_clicked)
-button.pack(side="left")
+# button.pack(side="left")
+button.grid(column=1,row=1)
+button2 = Button(text="New Button")
+button2.grid(column=2,row=0)
 
 #Entry
 input = Entry(width=10)
-input.pack(side="left")
+# input.pack(side="left")
+input.grid(column=3,row=3)
 
 window.mainloop()
