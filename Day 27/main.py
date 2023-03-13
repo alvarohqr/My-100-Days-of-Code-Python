05e76e51618d6a25e7b1954f378dcfeceaec5ea8
alvarohqr <alvarohqr@gmail.com>
2023-03-13 12:58:03 -0700

    Tkinter: widget padding

--- Day 27/main.py
+++ Day 27/main.py
@@ -6,15 +6,18 @@ def button_clicked():
 window = Tk()
 window.title("Tkinter GUI")
 window.minsize(480, 480)
+#Add more space around the window edges
+window.config(padx=200, pady=200)
 
 #Label
 my_label = Label(text="I'am a label", font=("Arial", 12, "bold"))
 my_label["text"] = "New Text"
-my_label.config(text="New Text 2")
+my_label.config(text="New Text 2", padx=50, pady=50)
 
 # my_label.pack(side="left")
 #my_label.place(x=100,y=200)
 my_label.grid(column=0, row=0)
+
 
 #Button
 button = Button(text="Click me", command=button_clicked)
