419cf4db326243e61494c4e10d68cea44015fa3f
alvarohqr <alvarohqr@gmail.com>
2023-03-13 12:44:06 -0700

    Tkinter: pack position

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,27 +1,24 @@
 from tkinter import *
 
+def button_clicked(): 
+    my_label.config(text=input.get())
+
 window = Tk()
 window.title("Tkinter GUI")
 window.minsize(480, 480)
 
 #Label
 my_label = Label(text="I'am a label", font=("Arial", 26, "bold"))
-my_label.pack()
-
 my_label["text"] = "New Text"
 my_label.config(text="New Text 2")
+my_label.pack(side="left")
+
+#Button
+button = Button(text="Click me", command=button_clicked)
+button.pack(side="left")
 
 #Entry
 input = Entry(width=10)
-input.pack()
-
-
-#Button
-def button_clicked(): 
-    my_label.config(text=input.get())
-
-button = Button(text="Click me", command=button_clicked)
-button.pack()
-
+input.pack(side="left")
 
 window.mainloop()
