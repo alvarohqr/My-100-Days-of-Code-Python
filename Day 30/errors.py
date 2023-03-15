fdc405a6282fb1d2c1fde1d18fc4ee67ad0cd853
alvarohqr <alvarohqr@gmail.com>
2023-03-15 12:08:18 -0700

    Raising your own Exceptions

--- Day 30/errors.py
+++ Day 30/errors.py
@@ -1,17 +1,25 @@
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError: 
-    file = open("Day 30/a_file.txt", "a")
-    file.write("\nSomething")
-except KeyError as error_message: 
-    print(f"Thay key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    # file.close()
-    # print("File was closed.")
-    raise KeyboardInterrupt("YO")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError: 
+#     file = open("Day 30/a_file.txt", "a")
+#     file.write("\nSomething")
+# except KeyError as error_message: 
+#     print(f"Thay key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print("File was closed.")
+#     raise KeyboardInterrupt("YO")
 
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise TypeError("Human height should not be over 3 meters")
+
+bmi = weight / height ** 2
+print(f"{bmi}")
