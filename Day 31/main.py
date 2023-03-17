b7374167ccdf13d36bf1b3905d225909a6d23b63
alvarohqr <alvarohqr@gmail.com>
2023-03-16 17:00:41 -0700

    Flash Card App

--- Day 31/main.py
+++ Day 31/main.py
@@ -4,13 +4,16 @@ from random import choice, randint
 WIDTH = 800
 HEIGHT = 526
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {} 
+to_learn = {}
 try:
-    data = pd.read_csv("Day 31/data/french_words.csv")
-except TypeError:
-    print("Not such file found.")
+    data = pd.read_csv("Day 31/data/words_to_learn.csv.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("Day 31/data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records") 
 else:       
     to_learn = data.to_dict(orient="records")  
-    current_card = {} 
+    
 #----------------------------------NEXT CARD----------------------------------
 def next_card():
     global current_card, flip_timer
@@ -22,11 +25,18 @@ def next_card():
     flip_timer = window.after(3000, func=flip_card)
 
 #----------------------------------FLIP CARD----------------------------------    
-
 def flip_card(): 
     canvas.itemconfig(card_title, text="Enlgish", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back) 
+
+#----------------------------------REMOVE CARD----------------------------------
+def is_known():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv("Day 31/data/words_to_learn.csv", index=False)
+    next_card()
+
 
 #----------------------------------UI SETUP----------------------------------
 window = Tk()
@@ -49,7 +59,7 @@ wrong_button = Button(image=wrong, background=BACKGROUND_COLOR, highlightthickne
 wrong_button.grid(column=0, row=1)
 
 right = PhotoImage(file="Day 31/images/right.png")
-right_button = Button(image=right, background=BACKGROUND_COLOR, highlightthickness=0, command=next_card) 
+right_button = Button(image=right, background=BACKGROUND_COLOR, highlightthickness=0, command=is_known) 
 right_button.grid(column=1, row=1)
 
 next_card()
