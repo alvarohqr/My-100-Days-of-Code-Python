3393d84df7a28aca49605c6de277fdaa1bbd6d1d
alvarohqr <alvarohqr@gmail.com>
2023-03-20 13:57:37 -0700

    Automated Birthday Wisher

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,20 +1,36 @@
+#To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
+# See the solution video in the 100 Days of Python Course for explainations.
+
+
+from datetime import datetime
+import pandas
+import random
 import smtplib
-import datetime as dt
 
-# my_email = "hr326157@gmail.com"
-# PASWORD = "unlrxswjkctvnucn"
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connect:
-#     #Transport layer security
-#     connect.starttls()
-#     connect.login(user=my_email, password=PASWORD)
-#     connect.sendmail(from_addr=my_email, 
-#                     to_addrs="elalvaro@yahoo.com", 
-#                     msg="Subject: TEST PY\n\nHola pa"
-#                     )
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-now = dt.datetime.now()
-year = now.year
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-date_of_birth = dt.datetime(year=1995, month=12, day=13, hour=11, minute=13)
-print(date_of_birth) 
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
