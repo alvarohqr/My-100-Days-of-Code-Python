de1a1801384b499729d7c345d4efa3a562627c4f
alvarohqr <alvarohqr@gmail.com>
2023-03-22 12:01:06 -0700

    ISS APi

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,12 +1,17 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
-if response.status_code != 200:
-    raise Exception("Bad response from ISS API")
+# if response.status_code != 200:
+#     raise Exception("Bad response from ISS API")
 
-response.raise_for_status()
+# response.raise_for_status()
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude) 
+
+url1="https://api.kanye.rest" 
+response = requests.get(url1)
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (longitude, latitude)
+print(data['quote'])
