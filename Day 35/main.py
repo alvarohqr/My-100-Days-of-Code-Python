8da01ebff36a20ddc42be27d280672ba775a3f4e
alvarohqr <alvarohqr@gmail.com>
2023-03-23 12:22:47 -0700

    Excluding data

--- Day 35/main.py
+++ Day 35/main.py
@@ -3,11 +3,14 @@ import requests
 API_KEY = "e2b5f5c48ac8cf5cacb4db9925b42256"
 URL = "https://api.openweathermap.org/data/3.0/onecall"
 
+# Just getting the hourly forecast
 parameters = {
     "lat": 27.496031,
     "lon": -109.932838,
-    "appid": API_KEY
+    "appid": API_KEY,
+    "exclud": "current,minutley,daily"
 }
 
 response = requests.get(url=URL, params=parameters)
 weather_data = response.json()
+print(weather_data)
